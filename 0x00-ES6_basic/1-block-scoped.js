8caea30eeba025db014c8f7afafcf9c6af2534df
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-07-24 09:20:43 +0000

    "Updated variable declarations and function syntax in 1-block-scoped.js and 2-arrow.js files."

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  task = false;
-  task2 = true;
-
-  if (trueOrFalse) {
-    var task;
-    var task2;
+    let task = false;
+    let task2 = true;
+  
+    if (trueOrFalse) {
+      let task = true;
+      let task2 = false;
+    }
+  
+    return [task, task2];
   }
-
-  return [task, task2];
-}
+  

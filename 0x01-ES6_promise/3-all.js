87351b1f8e16d863860119b3ee68d1c0bea611ee
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-07-28 21:47:20 +0000

    "Refactor handleProfileSignup function to use async/await syntax and update import order"

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,17 @@
 // 3-all.js
-import { uploadPhoto, createUser } from './utils';
+import { createUser, uploadPhoto } from "./utils";
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-    })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+async function handleProfileSignup() {
+  try {
+    const [photoResponse, userResponse] = await Promise.all([
+      uploadPhoto(),
+      createUser()
+    ]);
+
+    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  } catch (error) {
+    console.error("Signup system offline", error);
+  }
 }
 
 export default handleProfileSignup;

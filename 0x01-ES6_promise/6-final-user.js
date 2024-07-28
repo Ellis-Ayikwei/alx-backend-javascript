1863ecef141dacdb4a6c21d812972bde3e648874
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-07-28 22:28:42 +0000

    "Refactor handleProfileSignup function to use Promise.allSettled and return an array of results"

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,21 +1,16 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-const handleProfileSignup = async (firstName, lastName, fileName) => {
-  try {
-    const photoResponse = await uploadPhoto(fileName);
-    const userResponse = await signUpUser(firstName, lastName);
+export const handleProfileSignup = async (firstName, lastName, fileName) => {
+  const results = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]);
 
-    return {
-      status: 200,
-      value: `${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`,
-    };
-  } catch (error) {
-    return {
-      status: 400,
-      value: error,
-    };
-  }
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  }));
 };
 
 export default handleProfileSignup;

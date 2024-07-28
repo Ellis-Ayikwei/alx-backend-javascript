a81cb4171d93cd6bc58dca75168159f9f6821e46
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-07-28 21:56:43 +0000

    "Updated import/export quotes and console log quotes to single quotes"

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,16 @@
 // 3-all.js
-import { createUser, uploadPhoto } from "./utils";
+import { createUser, uploadPhoto } from './utils';
 
 async function handleProfileSignup() {
   try {
     const [photoResponse, userResponse] = await Promise.all([
       uploadPhoto(),
-      createUser()
+      createUser(),
     ]);
 
     console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
   } catch (error) {
-    console.log("Signup system offline");
+    console.log('Signup system offline');
   }
 }
 

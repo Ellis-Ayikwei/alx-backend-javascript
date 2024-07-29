b83f4ce8ba64e99480e49fe398b84ddedbde2916
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-07-29 00:09:14 +0000

    "Removed unnecessary whitespace and added closing bracket to divideFunction"

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,9 @@
 const divideFunction = (dividend, divisor) => {
-    if (divisor === 0) {
-      throw new Error('cannot divide by 0');
-    }
-
-    return dividend / divisor;
+  if (divisor === 0) {
+    throw new Error('cannot divide by 0');
   }
+
+  return dividend / divisor;
+};
 
 export default divideFunction;

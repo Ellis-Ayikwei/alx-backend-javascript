a86b536b0ca9090101b4432ba73d1c78d6ba1b6e
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-05 21:41:59 +0000

    Refactor cleanSet function to improve readability and handle invalid prefix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,17 @@
 const cleanSet = (inputSet, startString) => {
-	if (
-		typeof startString !== "string" ||
-		startString.length === 0 ||
-		!(inputSet instanceof Set)
-	) {
-		return "";
-	}
+  if (
+    typeof startString !== 'string'
+		|| startString.length === 0
+		|| !(inputSet instanceof Set)
+  ) {
+    return '';
+  }
 
-	const filteredItems = Array.from(inputSet)
-		.filter((item) => typeof item === "string" && item.startsWith(startString))
-		.map((item) => item.slice(startString.length));
+  const filteredItems = Array.from(inputSet)
+    .filter((item) => typeof item === 'string' && item.startsWith(startString))
+    .map((item) => item.slice(startString.length));
 
-	return filteredItems.join("-");
+  return filteredItems.join('-');
 };
 
 export default cleanSet;

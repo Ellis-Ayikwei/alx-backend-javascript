fb7a4cad0ca43ed05a928861ad64ff99d3d2ff45
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-05 20:47:42 +0000

    "Added prefix validation and return empty string if prefix is invalid; reformatted code"

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,16 @@
-const cleanSet = (set, prefix) => {
-  const filteredSet = [];
-  if (prefix) {
-    set.forEach((item) => {
-      if (item.startsWith(prefix)) {
-        filteredSet.push(item.slice(prefix.length));
-      }
-    });
-  }
-  return filteredSet.join('-');
+const cleanSet = (set, startString) => {
+	if (typeof startString !== "string" || startString.length === 0) {
+		return "";
+	}
+
+	const filteredSet = [];
+	set.forEach((item) => {
+		if (item.startsWith(startString)) {
+			filteredSet.push(item.slice(startString.length));
+		}
+	});
+
+	return filteredSet.join("-");
 };
 
 export default cleanSet;

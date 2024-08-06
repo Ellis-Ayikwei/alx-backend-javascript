52b60d93a9a1b96e777ee9981a1238da05380e29
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-06 15:52:39 +0000

    Refactor Pricing class to use getters and setters for price and currency

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+  constructor(price, currency) {
+    this._price = price;
+    this._currency = currency;
+  }
+
+  get price() {
+    return this._price;
+  }
+
+  set price(value) {
+    this._price = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._price} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}

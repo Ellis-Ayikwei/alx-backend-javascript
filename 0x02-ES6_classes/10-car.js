1434d8a7b2ac139edfdd30a90403c28d3a689d58
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-07 13:14:11 +0000

    Refactor Car class to use getters and setters for brand, model, and color properties

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,25 +1,35 @@
 export default class Car {
 	constructor(brand, model, color) {
-		this._brand = brand;
-		this._model = model;
-		this._color = color;
+		this.brand = brand;
+		this.model = model;
+		this.color = color;
 	}
 
 	get brand() {
 		return this._brand;
 	}
 
+	set brand(value) {
+		this._brand = value;
+	}
+
 	get model() {
 		return this._model;
+	}
+
+	set model(value) {
+		this._model = value;
 	}
 
 	get color() {
 		return this._color;
 	}
 
-	cloneCar() {
-		const Species = super.constructor[Symbol.species];
+	set color(value) {
+		this._color = value;
+	}
 
-		return new Species();
+	cloneCar() {
+		return new this.constructor();
 	}
 }

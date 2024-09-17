76366d29e6822fbb26a5068fa56fe74a0459b95c
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-17 12:52:25 +0000

    the app create server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+const app = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const server = app.createServer((req, res) => {
+  res.statusCode = 200;
+});
+
+server.listen(port, hostname, () => {
+  console.log('Hello Holberton School!');
+});

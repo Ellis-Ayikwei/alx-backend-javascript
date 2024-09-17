1572fee1fe51ff140c4ea31893778335051a80c1
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-17 12:28:04 +0000

    fixed the file to npm compatability

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,14 +1,9 @@
 const fs = require('fs');
 
-/**
- * Count students in a given file
- * @param {string} filename Path to the CSV file
- * @throws {Error} If the file cannot be loaded
- */
-function countStudents (filename) {
+function countStudents(filename) {
   try {
     const data = fs.readFileSync(filename, 'utf8');
-    const rows = data.split('\n').slice(1).filter(line => line.trim() !== '');
+    const rows = data.split('\n').slice(1).filter((line) => line.trim() !== '');
 
     const studentsByField = rows.reduce((acc, row) => {
       const field = row.split(',')[3];
@@ -20,9 +15,9 @@ function countStudents (filename) {
     console.log(`Number of students: ${rows.length}`);
     Object.entries(studentsByField).forEach(([field, names]) => {
       console.log(
-				`Number of students in ${field}: ${names.length}. List: ${names.join(
-					', '
-				)}`
+        `Number of students in ${field}: ${names.length}. List: ${names.join(
+          ', ',
+        )}`,
       );
     });
   } catch (err) {

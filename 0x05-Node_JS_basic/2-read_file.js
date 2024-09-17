481eeabce3fd38f22eb417050b2dcc3b2dab1078
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-17 12:16:43 +0000

    fixed the 2 file to the semistandard

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,33 +1,33 @@
-const fs = require("fs");
+const fs = require('fs');
 
 /**
  * Count students in a given file
  * @param {string} filename Path to the CSV file
  * @throws {Error} If the file cannot be loaded
  */
-function countStudents(filename) {
-	try {
-		const data = fs.readFileSync(filename, 'utf8');
-		const rows = data.split("\n").slice(1).filter(line => line.trim() !== "");
+function countStudents (filename) {
+  try {
+    const data = fs.readFileSync(filename, 'utf8');
+    const rows = data.split('\n').slice(1).filter(line => line.trim() !== '');
 
-		const studentsByField = rows.reduce((acc, row) => {
-			const field = row.split(",")[3];
-			const firstname = row.split(",")[0];
-			acc[field] = (acc[field] || []).concat([firstname]);
-			return acc;
-		}, {});
+    const studentsByField = rows.reduce((acc, row) => {
+      const field = row.split(',')[3];
+      const firstname = row.split(',')[0];
+      acc[field] = (acc[field] || []).concat([firstname]);
+      return acc;
+    }, {});
 
-		console.log(`Number of students: ${rows.length}`);
-		Object.entries(studentsByField).forEach(([field, names]) => {
-			console.log(
+    console.log(`Number of students: ${rows.length}`);
+    Object.entries(studentsByField).forEach(([field, names]) => {
+      console.log(
 				`Number of students in ${field}: ${names.length}. List: ${names.join(
-					", "
+					', '
 				)}`
-			);
-		});
-	} catch (err) {
-		throw new Error('Cannot load the database');
-	}
+      );
+    });
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;

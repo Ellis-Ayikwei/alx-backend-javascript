44c084136494a76b803260b43340616ea83ec83d
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-19 18:56:29 +0000

    remoed the it describes

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -6,19 +6,11 @@ describe("calculateNumber", function () {
 		it("should return the 8 if a is 5 b is 3", () => {
 			assert.strictEqual(calculateNumber("SUM", 5, 3), 8);
 		});
-
-		it("should return 3, a = 1.6, b = 2.3", () => {
-			assert.strictEqual(calculateNumber("SUM", 1.6, 2.3), 4);
-		});
 	});
 
 	describe("if the Type is MULTIPLY", () => {
 		it("should return the 15 if a is 5 b is 3", () => {
 			assert.strictEqual(calculateNumber("MULTIPLY", 5, 3), 15);
-		});
-
-		it("should return 3, a = 1.6, b = 2.3", () => {
-			assert.strictEqual(calculateNumber("MULTIPLY", 1.6, 2.3), 4);
 		});
 	});
 
@@ -26,19 +18,11 @@ describe("calculateNumber", function () {
 		it("should return the 5 if a is 15 b is 3", () => {
 			assert.strictEqual(calculateNumber("DIVIDE", 15, 3), 5);
 		});
-
-		it('should return the string "Error" a is 10, b is 0', () => {
-			assert.strictEqual(calculateNumber("DIVIDE", 0, 0), "Error");
-		});
 	});
 
 	describe("if the Type is SUBTRACT", () => {
 		it("should return the 2 if a is 5 b is 3", () => {
 			assert.strictEqual(calculateNumber("SUBTRACT", 5, 3), 2);
-		});
-
-		it("should return 3, a = 5.6, b = 2.3", () => {
-			assert.strictEqual(calculateNumber("SUBTRACT", 5.6, 2.3), 4);
 		});
 	});
 });

fed54cff5153b52de99c63c9c8750ad34b265535
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-19 19:05:30 +0000

    The testcases expanded

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,22 +1,82 @@
-const assert = require("assert");
-const calculateNumber = require("./1-calcul");
+const assert = require('assert').strict;
+const calculateNumber = require('./1-calcul');
 
-describe("calculateNumber", function () {
-	describe("if the Type is Sum", () => {
-		it("should return the 8 if a is 5 b is 3", () => {
-			assert.strictEqual(calculateNumber("SUM", 5, 3), 8);
-		});
-	});
+describe('calculateNumber', function () {
+  describe('SUM no Round', function () {
+    it('should return 5', function () {
+      assert.equal(calculateNumber('SUM', 1, 4), 5);
+    });
+  });
 
-	describe("if the Type is DIVIDE", () => {
-		it("should return the 5 if a is 15 b is 3", () => {
-			assert.strictEqual(calculateNumber("DIVIDE", 15, 3), 5);
-		});
-	});
+  describe('SUM first round', function () {
+    it('should return 6', function () {
+      assert.equal(calculateNumber('SUM', 2.4, 4), 6);
+    });
+  });
 
-	describe("if the Type is SUBTRACT", () => {
-		it("should return the 2 if a is 5 b is 3", () => {
-			assert.strictEqual(calculateNumber("SUBTRACT", 5, 3), 2);
-		});
-	});
+  describe('SUM second round ', function () {
+    it('should return 6', function () {
+      assert.equal(calculateNumber('SUM', 4, 2.4), 6);
+    });
+  });
+
+  describe('SUM both round', function () {
+    it('should return 6', function () {
+      assert.equal(calculateNumber('SUM', 1.4, 4.5), 6);
+    });
+  });
+
+  describe('SUBTRACT no round', function () {
+    it('should return 2', function () {
+      assert.equal(calculateNumber('SUBTRACT', 5, 3), 2);
+    });
+  });
+
+  describe('SUBTRACT first round', function () {
+    it('should return -3', function () {
+      assert.equal(calculateNumber('SUBTRACT', 2, 4.5), -3);
+    });
+  });
+
+  describe('SUBTRACT second round', function () {
+    it('should return 3', function () {
+      assert.equal(calculateNumber('SUBTRACT', 4.5, 2), 3);
+    });
+  });
+
+  describe('SUBTRACT both round', function () {
+    it('should return -4', function () {
+      assert.equal(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    });
+  });
+
+  describe('DIVIDE no round', function () {
+    it('should return 2', function () {
+      assert.equal(calculateNumber('DIVIDE', 8, 4), 2);
+    });
+  });
+
+  describe('DIVIDE first round', function () {
+    it('should return 5', function () {
+      assert.equal(calculateNumber('DIVIDE', 9.5, 2), 5);
+    });
+  });
+
+  describe('DIVIDE second round', function () {
+    it('should return 0.2', function () {
+      assert.equal(calculateNumber('DIVIDE', 2, 9.5), 0.2);
+    });
+  });
+
+  describe('DIVIDE both round', function () {
+    it('should return 0.2', function () {
+      assert.equal(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    });
+  });
+
+  describe('DIVIDE Error', function () {
+    it('should return Error', function () {
+      assert.equal(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+    });
+  });
 });

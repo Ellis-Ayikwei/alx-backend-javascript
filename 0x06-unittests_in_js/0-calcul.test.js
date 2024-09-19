29ccd88e21800d8fc16a825150c1929e7c3fe0d6
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-19 17:51:19 +0000

    changed the text to dots

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,32 +2,34 @@ const assert = require("assert");
 const calculateNumber = require("./0-calcul");
 
 describe("calculateNumber", () => {
-	it("should return 4 when passed 1 and 3", () => {
-		const result = calculateNumber(1, 3);
-		assert.strictEqual(result, 4);
-	});
+	describe(" the rounded parts", () => {
+		it("...", () => {
+			const result = calculateNumber(1, 3);
+			assert.strictEqual(result, 4);
+		});
 
-	it("should return 5 when passed 1 and 3.7", () => {
-		const result = calculateNumber(1, 3.7);
-		assert.strictEqual(result, 5);
-	});
+		it("...", () => {
+			const result = calculateNumber(1, 3.7);
+			assert.strictEqual(result, 5);
+		});
 
-	it("should return 5 when passed 1.2 and 3.7", () => {
-		const result = calculateNumber(1.2, 3.7);
-		assert.strictEqual(result, 5);
-	});
+		it("...", () => {
+			const result = calculateNumber(1.2, 3.7);
+			assert.strictEqual(result, 5);
+		});
 
-	it("should return 6 when passed 1.5 and 3.7", () => {
-		const result = calculateNumber(1.5, 3.7);
-		assert.strictEqual(result, 6);
-	});
+		it("...", () => {
+			const result = calculateNumber(1.5, 3.7);
+			assert.strictEqual(result, 6);
+		});
 
-	it("should return 6 when passed 1.5 and 3.7", () => {
-		const result = calculateNumber(1.5, 3.7);
-		assert.strictEqual(result, 6);
-	});
+		it("...", () => {
+			const result = calculateNumber(1.5, 3.7);
+			assert.strictEqual(result, 6);
+		});
 
-	after(() => {
-		console.log("tests done");
+		after(() => {
+			console.log("...");
+		});
 	});
 });

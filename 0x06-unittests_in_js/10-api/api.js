c6131da65412c70b89b4b99d7c280c37200bf4b0
Ellis-Ayikwei <126599954+Ellis-Ayikwei@users.noreply.github.com>
2024-09-20 19:45:54 +0000

    addeded the folder and the routes

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+const port = 7865;
+const localhost = "127.0.0.1";
+
+app.get("/", (_, res) => {
+	res.statusCode = 200;
+	res.contentType("text/plain");
+	res.send("Welcome to the payment system");
+});
+
+app.get("/cart/:id([0-9]+)", (req, res) => {
+	const id = req.params.id;
+	const regex = new RegExp("^[0-9]+$");
+
+	if (!regex.test(id)) {
+		res.statusCode = 404;
+		res.contentType("text/plain");
+		return res.send("Not Found");
+	}
+	res.statusCode = 200;
+	res.contentType("text/plain");
+	res.send(`Payment methods for cart ${id}`);
+});
+
+
+app.get('/available_payments', () => {
+	
+})
+
+const server = app.listen(port, localhost, () => {
+	console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = server;
---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,36 @@
+const request = require("request");
+
+const expect = require("chai").expect;
+
+describe("Index Page", () => {
+	it("should return the correct message", (done) => {
+		request.get("http://localhost:7865/", (error, res, body) => {
+			expect(res.statusCode).to.be.equal(200);
+			expect(res.headers["content-type"]).to.be.equal(
+				"text/plain; charset=utf-8"
+			);
+			expect(body).to.be.equal("Welcome to the payment system");
+			done();
+		});
+	});
+
+	it("should return Payment methods for cart :id", (done) => {
+		request.get("http://localhost:7865/cart/5", (error, res, body) => {
+			expect(res.statusCode).to.be.equal(200);
+			expect(res.headers["content-type"]).to.be.equal(
+				"text/plain; charset=utf-8"
+			);
+			expect(body).to.be.equal("Payment methods for cart 5");
+			done();
+		});
+	});
+	it("should return Not found", (done) => {
+		request.get("http://localhost:7865/cart/a", (error, res, body) => {
+			expect(res.statusCode).to.be.equal(404);
+			expect(res.headers["content-type"]).to.be.equal(
+				"text/html; charset=utf-8"
+			);
+			done();
+		});
+	});
+});

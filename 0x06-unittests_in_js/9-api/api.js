0066f9ebc39d83e046e08225b45a129b874cd775
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-20 18:51:37 +0000

    the payment messge

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const port = 7865;
+const localhost = "127.0.0.1";
+
+app.get("/", (_, res) => {
+	res.statusCode = 200;
+	res.contentType("text/plain");
+	res.send("Welcome to the payment system");
+});
+
+app.get("/:id", (req, res) => {
+	const id = req.params.id;
+	const regex = new RegExp("^[0-9]+$");
+
+	if (!regex.test(id)) {
+		res.statusCode = 404;
+		res.contentType("text/plain");
+		return res.send("Not Found");
+	}
+	res.statusCode = 200;
+	res.contentType("text/plain");
+	res.send(`Payment methods for cart ${id}`);
+});
+
+const server = app.listen(port, localhost, () => {
+	console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = server;

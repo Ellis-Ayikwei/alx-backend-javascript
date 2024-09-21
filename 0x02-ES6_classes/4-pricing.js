c5224a336a235bbbd7d2987ece707ee55cb44fd1
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-21 02:00:53 +0000

    the curreny

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,32 +1,31 @@
-import Currency from "./3-currency.js";
-
+import Currency from "./3-currency";
 export default class Pricing {
-	constructor(price, currency) {  // Changed Currency to currency for consistency
-		this._price = price;
-		this._currency = currency;
-	}
+  constructor(price, currency) {
+    this._price = price;
+    this._currency = currency;
+  }
 
-	get price() {
-		return this._price;
-	}
+  get price() {
+    return this._price;
+  }
 
-	set price(value) {
-		this._price = value;
-	}
+  set price(value) {
+    this._price = value;
+  }
 
-	get currency() {
-		return this._currency;
-	}
+  get currency() {
+    return this._currency;
+  }
 
-	set currency(value) {
-		this._currency = value;
-	}
+  set currency(value) {
+    this._currency = value;
+  }
 
-	displayFullPrice() {
-		return `${this._price} ${this._currency.displayFullCurrency()}`;
-	}
+  displayFullPrice() {
+    return `${this._price} ${this._currency.displayFullCurrency()}`;
+  }
 
-	static convertPrice(amount, conversionRate) {
-		return amount * conversionRate;
-	}
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }

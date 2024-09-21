a5d8242b07b3c8fda8f6dd8bacf6a89327fbeaa0
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-21 01:52:31 +0000

    the 4th file

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,71 +1,32 @@
-import Currency from "./3-currency.js";
+import Currency from './3-currency';
 
 export default class Pricing {
-	/**
-	 * Creates an instance of Pricing.
-	 * @param {Number} amount
-	 * @param {Currency} currency
-	 * @memberof Pricing
-	 */
-	constructor(amount, currency) {
-		this._amount = amount;
-		this._currency = currency;
-	}
+  constructor(amount) {
+    this._amount = amount;
+    this._currency = Currency;
+  }
 
-	/**
-	 * Gets the amount.
-	 * @readonly
-	 * @memberof Pricing
-	 */
-	get amount() {
-		return this._amount;
-	}
+  get amount() {
+    return this._amount;
+  }
 
-	/**
-	 * Sets the amount.
-	 * @param {Number} value
-	 * @memberof Pricing
-	 */
-	set amount(value) {
-		this._amount = value;
-	}
+  set amount(value) {
+    this._amount = value;
+  }
 
-	/**
-	 * Gets the currency.
-	 * @readonly
-	 * @memberof Pricing
-	 */
-	get currency() {
-		return this._currency;
-	}
+  get currency() {
+    return this._currency;
+  }
 
-	/**
-	 * Sets the currency.
-	 * @param {Currency} value
-	 * @memberof Pricing
-	 */
-	set currency(value) {
-		this._currency = value;
-	}
+  set currency(value) {
+    this._currency = value;
+  }
 
-	/**
-	 * Converts a price with a conversion rate.
-	 * @static
-	 * @param {Number} amount
-	 * @param {Number} conversionRate
-	 * @returns {Number}
-	 * @memberof Pricing
-	 */
-	static convertPrice(amount, conversionRate) {
-		return amount * conversionRate;
-	}
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 
-	/**
-	 * Displays the full price.
-	 * @returns {String}
-	 * @memberof Pricing
-	 */
-	displayFullPrice() {
-		return `${this._amount} ${this._currency.displayFullCurrency()}`;
-	}
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
 }
